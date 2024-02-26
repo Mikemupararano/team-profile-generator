b1c006499b59b77720f6ce2df378b465fa8dcebc
Mikemupararano <kudath@yahoo.co.uk>
2024-02-26 16:42:54 +0000

    Made the HTML responsive.

--- starter/src/page-template.js
+++ starter/src/page-template.js
@@ -1,60 +1,59 @@
 // creates the team
 const generateTeam = team => {
-
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header bg-primary text-white">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        <div class="card employee-card mx-3 my-3">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
         </div>
-        <div class="card-body mx-auto">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
         `;
     };
 
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card text-primary">
-    <div class="card-header bg-primary text-white">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body mx-auto">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
+        <div class="card employee-card text-primary mx-3 my-3">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
+        </div>
         `;
     };
 
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card text-primary">
-    <div class="card-header bg-primary text-white">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body mx-auto">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
+        <div class="card employee-card text-primary mx-3 my-3">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
         `;
     };
 
@@ -76,43 +75,49 @@ const generateTeam = team => {
     );
 
     return html.join("");
-
-}
+};
 
 // exports function to generate entire page
 module.exports = team => {
-
     return `
     <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-
-<body>
-    <div class="container-fluid text-primary">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading bg-info text-primary">
-                <h1 class="text-center bg-danger text-white py-4">My Team</h1>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <!-- Updated viewport meta tag for responsiveness -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <!-- Added Bootstrap CSS link -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="../style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+        <style>
+            /* Custom styles for smaller screens */
+            @media (max-width: 576px) {
+                .employee-card {
+                    margin-bottom: 20px;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid text-primary">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading bg-info text-primary mx-auto">
+                    <h1 class="text-center bg-danger text-white py-4">My Team</h1>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container text-primary mx-auto">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center text-primary mx-automate">
-                ${generateTeam(team)}
+        <div class="container text-primary">
+            <div class="row">
+                <div class="team-area col-12 d-flex flex-wrap justify-content-center text-primary mx-auto">
+                    ${generateTeam(team)}
+                </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
+    </body>
+    </html>
     `;
 };
